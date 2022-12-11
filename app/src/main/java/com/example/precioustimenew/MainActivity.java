b27fd5908d441cf85a1f1b7ca74e6e53330c23be
shoham <shoham.co777@gmail.com>
2022-12-11 17:51:51 +0200

    first screen buttons works

--- app/src/main/java/com/example/precioustimenew/MainActivity.java
+++ app/src/main/java/com/example/precioustimenew/MainActivity.java
@@ -1,26 +1,63 @@
 package com.example.precioustimenew;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    /*
     public EditText personalNumber;
     public EditText id;
     public EditText password;
     public Button login;
     public Button signup;
-
+*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {    //what happens once the first screen up
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        personalNumber = findViewById(R.id.personalNumberText);
-        id = findViewById(R.id.idText);
-        password = findViewById(R.id.passwordText);
-        login = findViewById(R.id.loginButton);
-        signup = findViewById(R.id.signUpButton);
+        TextView personalNumber = (TextView) findViewById(R.id.personalNumberText);
+        TextView id = (TextView) findViewById(R.id.idText);
+        TextView password = (TextView) findViewById(R.id.passwordText);
+
+        Button commander_login = (Button)findViewById(R.id.commander_login_Button);
+        commander_login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this, commanderMenu.class));
+
+            }
+        });
+
+        Button warrior_login = (Button)findViewById(R.id.warrior_login_Button);
+        warrior_login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this, soldierMenu.class));
+
+            }
+        });
+
+        Button signup = (Button)findViewById(R.id.signUpButton);
+        signup.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this, signUpActivity.class));
+
+            }
+        });
+
+
     }
 
 }
